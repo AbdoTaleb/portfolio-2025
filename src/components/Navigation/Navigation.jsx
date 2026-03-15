88a1f6cd64a41f2e391b9aca80fabea7d5338b19
Abdo Taleb <44838067+AbdoTaleb@users.noreply.github.com>
2026-03-16 00:31:16 +0100

    fixing work page

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,18 +4,66 @@ import { Link, useLocation } from "react-router-dom";
 
 const Navigation = () => {
   const { pathname } = useLocation();
+
   const navClass = pathname === "/work" || pathname === "/projects" || pathname === "/education" || pathname === "/contact" ? "white-nav" : "black-nav";
 
   return (
     <nav className={`main-nav ${navClass}`}>
       <div className="nav-wrapper">
         <div className="nav-home">
-          <Link
-            to="/"
-            className="vertical-text"
-          >
-            HOME
-          </Link>
+          {pathname !== "/" && (
+            <Link
+              to="/"
+              className="vertical-text"
+            >
+              HOME
+            </Link>
+          )}
+
+          {pathname !== "/work" && (
+            <Link
+              to="/work"
+              className="vertical-text"
+            >
+              WORK
+            </Link>
+          )}
+
+          {pathname !== "/projects" && (
+            <Link
+              to="/projects"
+              className="vertical-text"
+            >
+              PROJECTS
+            </Link>
+          )}
+
+          {pathname !== "/education" && (
+            <Link
+              to="/education"
+              className="vertical-text"
+            >
+              EDUCATION
+            </Link>
+          )}
+
+          {pathname !== "/contact" && (
+            <Link
+              to="/contact"
+              className="vertical-text"
+            >
+              CONTACT
+            </Link>
+          )}
+
+          {pathname !== "/about" && (
+            <Link
+              to="/about"
+              className="vertical-text"
+            >
+              ABOUT
+            </Link>
+          )}
 
           <div className="nav-line" />
         </div>
