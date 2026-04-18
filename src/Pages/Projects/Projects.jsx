37d62c3ba809b6612733dba54b24ffbf7acf3a97
Abdo Taleb <44838067+AbdoTaleb@users.noreply.github.com>
2026-04-18 10:33:44 +0200

    add The Wild Oasis

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,75 +1,98 @@
-import React from 'react'
-import './Projects.scss'
-import { useState, useEffect } from 'react'
-import Navigation from '../../components/Navigation/Navigation';
-
+import React from "react";
+import "./Projects.scss";
+import { useState, useEffect } from "react";
+import Navigation from "../../components/Navigation/Navigation";
 
 function Projects() {
-    const [animate, setAnimate] = useState(false); 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setAnimate(true);  
-        }, 100); 
-        return () => clearTimeout(timer);
-    }, []);
+  const [animate, setAnimate] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAnimate(true);
+    }, 100);
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <section className={`projects-page ${animate ? 'animate' : ''}`}>
-        {/* <Navigation /> */}
-        <div className="background-overlay"></div>
-      <div className='left-side fade-in'>
+    <section className={`projects-page ${animate ? "animate" : ""}`}>
+      {/* <Navigation /> */}
+      <div className="background-overlay"></div>
+      <div className="left-side fade-in">
         <p className="label">Projects</p>
         <p className="desc">Here you can find a selection of my projects, showcasing my skills in frontend development and design.</p>
         <p className="desc">Feel free to explore and reach out if you have any questions or feedback!</p>
       </div>
 
-      <div className='right-side fade-in'>
+      <div className="right-side fade-in">
         <ul className="project-list">
-          <li className='project-item'>
-                <h2>SneakX E-Commerce</h2>
-                <span>Sneaker store: product listing with images, pagination, and product details.
-                      Backend hosted on Render (.NET 8 Web API) with Azure SQL for data.</span> <br />
-                <span><strong>Technologies:</strong> React, SCSS, Axios, ASP.NET Core Web API, SQL Server (Azure)</span>
-                <a 
-                  href="https://sneakx-frontend.vercel.app/" 
-                  target="_blank" 
-                  rel="noreferrer"
-                  className="project-visit-btn"
-                >
-                  Visit Project
-                </a>
+          <li className="project-item">
+            <h2>The Wild Oasis</h2>
+            <span>
+              The Wild Oasis: full-stack app with authentication, API routes, and dynamic data handling using Next.js. Includes booking logic and backend
+              functionality.
+            </span>{" "}
+            <br />
+            <span>
+              <strong>Technologies:</strong> Next.js, Tailwind CSS, API Routes, Supabase
+            </span>
+            <a
+              href="https://the-wild-oasis-git-main-abdotalebs-projects.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="project-visit-btn"
+            >
+              Visit Project
+            </a>
+          </li>
+          <li className="project-item">
+            <h2>SneakX E-Commerce</h2>
+            <span>
+              Sneaker store: product listing with images, pagination, and product details. Backend hosted on Render (.NET 8 Web API) with Azure SQL for data.
+            </span>{" "}
+            <br />
+            <span>
+              <strong>Technologies:</strong> React, SCSS, Axios, ASP.NET Core Web API, SQL Server (Azure)
+            </span>
+            <a
+              href="https://sneakx-frontend.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="project-visit-btn"
+            >
+              Visit Project
+            </a>
+          </li>
 
-            </li>
-            
-            <li className='project-item'>
-                <h2>Munamii Cakery</h2>
-                <span>A fully responsive online cake shop built with React, featuring product filtering and cart functionality.</span> <br />
-                <span><strong>Technologies:</strong> React, SCSS, JavaScript</span>
-                <a 
-                  href="https://munamii-cakery.vercel.app/" 
-                  target="_blank" 
-                  rel="noreferrer"
-                  className="project-visit-btn"
-                >
-                  Visit Project
-                </a>
+          <li className="project-item">
+            <h2>Munamii Cakery</h2>
+            <span>A fully responsive online cake shop built with React, featuring product filtering and cart functionality.</span> <br />
+            <span>
+              <strong>Technologies:</strong> React, SCSS, JavaScript
+            </span>
+            <a
+              href="https://munamii-cakery.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="project-visit-btn"
+            >
+              Visit Project
+            </a>
+          </li>
 
-            </li>
-
-            <li className='project-item'>
-                <h2>E-Plant Shopping</h2>
-                <span>A modern plant e-commerce app allowing users to explore, search, and buy plants with ease.</span> <br />
-                <span><strong>Technologies:</strong> React, Redux Toolkit, Styled Components</span>
-                <a 
-                  href="https://e-plant-shopping-ashen.vercel.app/" 
-                  target="_blank" 
-                  rel="noreferrer"
-                  className="project-visit-btn"
-                >
-                  Visit Project
-                </a>
-
-            </li>
-            <li className="project-item">
+          <li className="project-item">
+            <h2>E-Plant Shopping</h2>
+            <span>A modern plant e-commerce app allowing users to explore, search, and buy plants with ease.</span> <br />
+            <span>
+              <strong>Technologies:</strong> React, Redux Toolkit, Styled Components
+            </span>
+            <a
+              href="https://e-plant-shopping-ashen.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="project-visit-btn"
+            >
+              Visit Project
+            </a>
+          </li>
+          <li className="project-item">
             <h2>Enhanced Drone Autopilot</h2>
             <span>– School work – Python / Hardware Integration</span>
           </li>
@@ -81,12 +104,10 @@ function Projects() {
             <h2>Knowit Karlskrona</h2>
             <span>– Backend Developer – Python (Flask) + React</span>
           </li>
-
         </ul>
-        
       </div>
     </section>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
